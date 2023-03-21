836948c30110372e1e8f59e262aa38723d23f650
Frederik De Bleser <frederik@debleser.be>
2023-03-21 14:57:22 +0100

    Initial commit.

---
+++ src/components/VideoPreview.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+defineProps({
+  video: String,
+})
+
+
+
+</script>
+
+<template>
+    <video controls muted autoplay>
+        <source :src="video" type="video/mp4" >
+    </video>    
+</template>
+
+<style scoped>
+video {
+    max-width: 100%;
+}
+
+
+</style>
